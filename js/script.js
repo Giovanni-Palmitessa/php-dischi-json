facf1f321faadb0a7cb05234d2adbb6c7ee1f68b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-09 18:19:06 +0200

    add method to select card

--- js/script.js
+++ js/script.js
@@ -10,7 +10,10 @@ const app = Vue.createApp({
             console.log('ciao');
             axios.get('datas.php')
                 .then(response => this.albums = response.data);
-        }
+        },
+        selectAlbum(album) {
+            this.selectedAlbum = album;
+          },
     },
     created() {
         this.requestAlbums();
