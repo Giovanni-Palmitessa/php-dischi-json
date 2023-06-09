bf8fab2126d6dd9ff79c84173094d8b52420066b
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-09 16:17:08 +0200

    add second item in album data

--- script/script.js
+++ script/script.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
                     img_URL = "assets/img/aquietplace.jpeg",
                     album_name = "New Jersey",
                     artist = "Bon Jovi",
-                    year = "1988"
+                    year = "1988",
+                ],
+                [
+                    img_URL = "assets/img/metal_lifting.jpg",
+                    album_name = "Live at Wembley 86",
+                    artist = "Queen",
+                    year = "1992",
                 ],
             ]
         }
