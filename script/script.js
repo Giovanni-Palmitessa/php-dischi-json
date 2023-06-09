36773c88a9a934f4971117b0a6a7d01537a34925
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-09 17:26:31 +0200

    add albums array

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
 const app = Vue.createApp({
     data() {
         return{
-            
+            albums: [],
         }
     }, 
     methods: {
         requestAlbums() {
-            axios.get('datas.php')
-                .then(response => console.log(response.data));
+            axios.get('http://localhost/php-dischi-json/datas.php')
+                .then(response => this.albums = response.data);
         }
     },
     created() {
