b0fd05920cb9a891c99b0df6642793cac21d6e44
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-09 16:15:40 +0200

    add array albums for data

--- script/script.js
+++ script/script.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
     data() {
         return{
-            
+            albums: [
+                [
+                    img_URL = "assets/img/aquietplace.jpeg",
+                    album_name = "New Jersey",
+                    artist = "Bon Jovi",
+                    year = "1988"
+                ],
+            ]
         }
     }
 });
